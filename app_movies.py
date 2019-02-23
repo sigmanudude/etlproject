3f054c8a62e56e487037d61a06b3ca826c3be4fd
Madhuram Swaminathan <madhuananth04@icloud.com>
2019-02-23 00:18:36 -0600

    Flask and HTML update

---
+++ app_movies.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+# from mission_to_mars import scrape
+import movies_func_lib as mfn
+import os
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/moviesdb")
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    # db_data = mongo.db.mars_news.find_one()
+
+        
+    # Return template and data
+    return render_template("index.html")
+
+
+# Route that will trigger the TMDB data page
+@app.route("/TMDB_data")
+def tmdb_data():
+
+    # Run the scrape function and save the results to a variable
+    htm = mfn.renderTMDBData_html()
+
+    return render_template("TMDB-Data.htm", tmdb_htm = htm)
+
+# Route that will trigger the TMDB data page
+@app.route("/OMDB_data")
+def omdb_data():
+
+    # Run the scrape function and save the results to a variable
+    htm = mfn.renderOMDBData_html()
+
+    return render_template("OMDB-Data.htm", omdb_htm = htm)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ movies_func_lib.py
@@ -0,0 +1,69 @@
+# This is a utilities file that will render data to flask app file
+
+# Import dependencies
+import pandas as pd 
+from pymongo import MongoClient
+
+import os
+
+
+#Initialize Mongodb connection
+conn = "mongodb://localhost:27017/"
+
+#establish connection
+client = MongoClient(conn)
+
+#connect to DB and collections
+db = client.moviesdb
+
+# connect to the collections
+omdb_coll = db.omdb_api
+tmdb_coll = db.tmdb_data
+
+# function to render the TMDB data as dataframe
+def renderTMDBData():
+    tmdb_df = []
+    for doc in tmdb_coll.find():
+        tmdb_df.append(doc)
+    
+    # add it to data frame
+    tmdb_df = pd.DataFrame(tmdb_df)
+    
+    return tmdb_df
+
+# function to render the TMDB data as dataframe
+def renderTMDBData_html():
+    tmdb_df = []
+    for doc in tmdb_coll.find():
+        tmdb_df.append(doc)
+    
+    # add it to data frame
+    tmdb_df = pd.DataFrame(tmdb_df)
+    
+    #convert dataframe to HTML
+    tmdb_htm = tmdb_df.to_html(classes = ['table table-striped table-hover'])
+    return tmdb_htm
+
+# function to render the TMDB data as dataframe
+def renderOMDBData():
+    omdb_df = []
+    for doc in omdb_coll.find():
+        omdb_df.append(doc)
+    
+    # add it to data frame
+    omdb_df = pd.DataFrame(omdb_df)
+    
+    return omdb_df
+
+# function to render the TMDB data as dataframe
+def renderOMDBData_html():
+    omdb_df = []
+    for doc in omdb_coll.find():
+        omdb_df.append(doc)
+    
+    # add it to data frame
+    omdb_df = pd.DataFrame(omdb_df)
+    
+    #convert dataframe to HTML
+    omdb_htm = omdb_df.to_html(classes = ['table table-striped table-hover'])
+    return omdb_htm
